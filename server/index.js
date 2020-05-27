7d1cbecd3092ce06f9b7543804057c85045f07f0
jatinarora135 <47227050+jatinarora135@users.noreply.github.com>
2020-05-27 08:47:50 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 //jshint esversion:6
 
 const express = require('express');
-const path = require('path');
 const app = express();
+const path = require('path');
 const renderToString =require( 'react-dom/server');
 var React=require( "react");
 var ReactDOM=require("react-dom");
@@ -10,6 +10,6 @@ app.use(express.static(path.join(__dirname, "..","build")));
 app.use(express.static("public"));
 const port = process.env.PORT || 6000;
 app.listen(port);
-console.log('Express Server Listening on port:' + port);
+console.log('Server started on port :' + port);
 var startChrome = require("start-chrome");
 startChrome("http://localhost:6000");
