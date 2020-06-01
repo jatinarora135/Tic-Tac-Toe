6a5041c9493a910c81a88a8a7477e5b1cd7122f3
jatinarora135 <jatinarora135@gmail.com>
2020-06-01 10:53:08 +0530

    latest

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ const bodyParser = require("body-parser");
 const request = require("request");
 
 const app=express();
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended : true}));
+app.get("/",function(req,res)
+{
+    res.sendFile(__dirname +"/signin.html");
+});
+app.get("/signup",function(req,res)
+{
+    res.sendFile(__dirname +"/signup.html");
+});
+app.post("/",function(req,res)
+{
+  var email=req.body.email;
+  var password=req.body.pass;
+  console.log(email,password);
+});
 app.listen(3000,function(){
     console.log("server is running at prt 3000");
 
